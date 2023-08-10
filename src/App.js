660620a38af442cc6e19697b52941903d5fe5688
CatExposure <andrenewbauer@gmail.com>
2023-08-10 14:37:52 -0400

    added 404 page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Home';
 import Databases from './Database_Usage';
 import API from './API_Usage';
 import Contact from './Contact';
+import PageNotFound from './404Page'
 import NavBar from './NavBar';
 import {Route, Link, Routes} from 'react-router-dom';
 
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Routes>
+        <Route exact path="*" Component={PageNotFound}/>
         <Route path="/" Component={Home}/>
         <Route path="/Contact" Component={Contact}/>
         <Route path="/API" Component={API}/>
