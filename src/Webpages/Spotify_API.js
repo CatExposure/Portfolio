647389dac4cafc11917d017027ffed6edb35740f
CatExposure <andrenewbauer@gmail.com>
2023-09-29 20:37:06 -0400

    removed error whenever reload occured, as well as made search system more intuitive by removeing search button

--- src/Webpages/Spotify_API.js
+++ src/Webpages/Spotify_API.js
@@ -6,7 +6,6 @@ import axios from 'axios';
 /*
 TO DO:
     change artist to songs if possible, mainly with the ability to play music, as well as show song name
-    find out axios problem when going back to page or reloading page
 */
 function SpotifyAPI(){
     const CLIENT_ID = "b0fddc430d1245ec9a363bee851354d8"; //identifier of the created app
@@ -17,7 +16,26 @@ function SpotifyAPI(){
     const [token, setToken] = useState("");
     const [searchKey, setSearchKey] = useState("");
     const [artists, setArtists] = useState([]);
-    const [Results, setResults] = useState(true)
+    const [Results, setResults] = useState("")
+    const [Error, setError] = useState("");
+    const error400 = "Sorry! looks like the request/response failed! (error 400)";
+    const error401 = "Sorry! looks like you have an invalid token (tokens expire after 1 hour!) (error 401)";
+    const errorElse = "Sorry! an unknown error has occured :(";
+
+    //if the user entered nothing or uses the * character (explained more later) then set the results state to false/none and prevents the search from running
+        //as for the * character, in the actual spotify app you can search with the * character, however the API seems to despise it.
+        //I don't have enough knowledge on how their search params handle *, so we will refrain from using it for now
+    useEffect(()=> {
+        if (searchKey.trim() === "" ) {
+            setResults("none");
+            return;
+        } else if(searchKey.startsWith("*")) {
+            setResults("false");
+            return;
+        }
+        window.sessionStorage.setItem("userSearch", searchKey);
+        getArtists();
+    }, [searchKey]);
 
     useEffect(()=> {
         //obtains the hash from our current webpage as well as token from our local storage (if we have one)
@@ -27,7 +45,6 @@ function SpotifyAPI(){
 
         if (token && userSearch) {
             setSearchKey(userSearch);
-            getArtists();
         }
         //if there is no token stored in the local storage (returned null) AND there is no hash...
         if(!token && hash) {
@@ -55,22 +72,8 @@ function SpotifyAPI(){
 
     //we use axios as it is more secure (prevents xsrf), but functions similarly to a fetch request
     //we use an async function with await to ensure that no further code is executed before the fetch request is complete
+    //we also use a try catch statement to ensure the webpage does not crash as well as provide error responses
     //lastly, we set the state of artists to an array of all the items for each artist in the data we fetched
-    const searchVerify = async (e) => {
-        e.preventDefault();
-
-        //if the user entered nothing or uses the * character (explained more later) then set the results state to false and prevents the search from running
-        //as for the * character, in the actual spotify app you can search with the * character, however the API seems to despise it.
-        //I don't have enough knowledge on how their search params handle *, so we will refrain from using it for now
-        if (searchKey.trim() === "" || searchKey.startsWith("*")) {
-            setResults(false);
-            return;
-        }
-        window.sessionStorage.setItem("userSearch", searchKey);
-        getArtists()
-    }
-
-
     const getArtists = async () => {
         try{
             const {data} = await axios.get("https://api.spotify.com/v1/search", {
@@ -85,18 +88,27 @@ function SpotifyAPI(){
 
         //if the user enteres a value and there are no artists, set the Results state to false, and set to true in any other case (if there's at least 1 artist)
         if (data.artists.items.length === 0) {
-            setResults(false);
+            setResults("none");
         } else {
-            setResults(true);
+            setResults("true");
         }
         console.log(data);
+        setError("");
         setArtists(data.artists.items);
 
     }catch(error){
+        if (error.request.status === 400) {
+            setError("400");
+        } else if (error.request.status === 401) {
+            setError("401");
+        } else {
+            setError("Unknown");
+        }
         console.log(error);
     } 
     }
 
+    //renders the genres of a particular artist in case they have more than one genre
     const renderGenres = (item) => {
         var genreList = "";
         item.genres.forEach(genre => {
@@ -108,10 +120,31 @@ function SpotifyAPI(){
         return genreList;
     }
 
+    //renders an error message depending on the error code
+    const renderError = () => {
+        if (Error === "400") {
+            return(
+            <div>{error400}</div>
+            )
+        } else if (Error === "401") {
+            return(
+            <div>{error401}</div>
+            )
+        } else if (Error === "Unknown"){
+            return(
+            <div>{errorElse}</div>
+            )
+        } else {
+            return(
+            <div></div>
+            )
+        }
+    }
+
     //maps out the artists and gives each artists a div with their id as the key, then includes an image and their name.
     //if the Results state is false, it will instead show no artists as this state would only be false if the search were to give an error or no artists
     const renderArtists = () => {
-        if (Results === false) {
+        if (Results === "false" || Results === "none") {
             return (
                 <div></div>
             )
@@ -134,7 +167,7 @@ function SpotifyAPI(){
 
     //will render a message stating there were no reults and to inform the user to refrain from using * if the Results state is false
     const renderResultsMessage = () => {
-        if (Results === false) {
+        if (Results === "false") {
             return (
                 <div>
                    <p>No results came up!</p>
@@ -165,16 +198,15 @@ function SpotifyAPI(){
         }
     }
 
-    //displays the search text input and button, as well as a message informing the user to login to use the spoitfy API if they are not
-    //also displays the ResultsMessage and Artists
+    //displays the search text input as well as a message informing the user to login to use the spoitfy API if they are not
+    //also displays all the render components
     return(
         <div>
             <h1>Spotify!</h1>
             {LoginOut()}
             {token ? 
-                <form onSubmit={searchVerify}> 
-                    <input type="text" onChange={e => setSearchKey(e.target.value)}/>
-                    <button type={"submit"}>Search</button>
+                <form> 
+                    <p>Search here: </p><input type="text" onChange={e => setSearchKey(e.target.value)}/>
                 </form>
 
                 : <h2>Please login to use search feature</h2>
@@ -183,6 +215,7 @@ function SpotifyAPI(){
 
             </div>
             {renderResultsMessage()}
+            {renderError()}
             <div className="artistResults">
                 {renderArtists()}
             </div>
