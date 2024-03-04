50db66a47c6ff706e4987f1621fde93d03df3d6a
CatExposure <andrenewbauer@gmail.com>
2024-03-04 15:20:07 -0500

    added .env file

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const oracledb = require('oracledb');
+const express = require('express');
+const app = express();
+require("dotenv").config();
+app.listen(3000);
 
 async function dbConnect(){
     let connect;
-
     try {
         connect = await oracledb.getConnection( {
-            user: "ADMIN",
-            password: "Megumin5656!",
-            connectString: "(description= (retry_count=2)(retry_delay=2)(address=(protocol=tcps)(port=1522)(host=adb.us-ashburn-1.oraclecloud.com))(connect_data=(service_name=gf0f94ceafe056c_sqlportfolio_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))"
+            user: process.env.USER,
+            password: process.env.PASSWORD,
+            connectString: process.env.CONNECTSTRING
         });
     } catch (err) {
         console.log(err)
     } finally {
         if (connect) {
             console.log("connection successful!")
+
         } else {
             console.log("connection unsuccessful :(")
         }
