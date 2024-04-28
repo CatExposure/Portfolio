1377c63eea3b2a1289a138f6307078ef8864f1c1
CatExposure <andrenewbauer@gmail.com>
2024-04-28 16:11:23 -0400

    added basics of tanstack and some basic explanation

--- Client/src/Webpages/DatabaseUsage.js
+++ Client/src/Webpages/DatabaseUsage.js
@@ -1,11 +1,109 @@
-import React from 'react'
-import axios from 'axios'
+import "../styles/Database.css";
+import React from 'react';
+import Axios from 'axios';
+import {flexRender, getCoreRowModel, useReactTable} from '@tanstack/react-table';
 
 function Databases(){
+
+     //runs only on first load. Only used to automatically fetch the database data and store it
+     React.useEffect(() => {
+        try {
+            Axios.get("/test").then((response) => {
+                ObjectConversion(response.data.rows);
+                //setarrayData(response.data.rows);
+            })
+        } catch (err){
+            console.log(err);
+        }
+    }, [])
+
+    //useState to store the data obtained from the database
+    const [data, setData] = React.useState([]);
+
+    //converts an array of arrays into an array of objects, because god forbid I can't use an array index as the accessorKey 
+    function ObjectConversion(arrayData){
+        var data = []
+        if (arrayData){
+            console.log(arrayData);
+            arrayData.forEach(row => {
+                var object = {
+                    "first_name": row[1],
+                    "last_name": row[2],
+                    "email": row[3],
+                    "address": row[4],
+                    "phone": row[5],
+                    "access": row[6],
+                    "password": row[7]
+                }
+                data.push(object);
+            });
+            setData(data)
+        }
+    }
+
+    //defines what the columns will display and what type of information is tied to them
+    const columns = [
+        {
+            accessorKey: "first_name",
+            header: "First Name",
+        },
+        {
+            accessorKey: "last_name",
+            header: "Last Name",
+        },
+        {
+            accessorKey: "email",
+            header: "Email Address",
+        },
+        {
+            accessorKey: "address",
+            header: "Address",
+        },
+        {
+            accessorKey: "phone",
+            header: "Phone Number",
+        },
+        {
+            accessorKey: "access",
+            header: "Access Level",
+        },
+        {
+            accessorKey: "password",
+            header: "Password",
+        },
+    ]
+    //columns and data are required options, while getCoreRowModel allows filtering, sorting, etc.
+    const table = useReactTable({
+        columns,
+        data,
+        getCoreRowModel:getCoreRowModel()
+    })
+
     return(
         <div>
             <h1>Databases</h1>
-            <p>{axios.get("http://localhost:3000/test")}</p>
+            <table className='table-container'>
+                <tbody>
+                {table.getHeaderGroups().map(headerGroup => <tr className = "tr" key={headerGroup.id}>
+                    {headerGroup.headers.map(
+                        header => <th className="th" key={header.id}>
+                            {header.column.columnDef.header}
+                        </th>
+                    )}
+                </tr>)}
+                {//getRowModel().rows returns an array of rows
+                table.getRowModel().rows.map(row => <tr className = "tr" key={row.id}>
+                {row.getVisibleCells().map(cell => <td className="td" key={cell.id}>
+                        {//look more into this
+                        flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                        )}
+                    </td>)}
+                </tr>)
+            }
+            </tbody>
+            </table>
         </div>
     )
 }
--- Client/src/Webpages/Home.js
+++ Client/src/Webpages/Home.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
 
 function Home(){
-    let test = Axios.get("/test")
-    console.log(test)
     return(
         <div>
             <h1>Home Page Test</h1>
