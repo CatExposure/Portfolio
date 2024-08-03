8336ea923b985f4cea1a8dc694e4cd8716d7175e
CatExposure <andrenewbauer@gmail.com>
2024-08-03 12:39:32 -0400

    Made NavBar responsive

--- Client/src/Components/Carousel.js
+++ Client/src/Components/Carousel.js
@@ -51,14 +51,14 @@ function Carousel(){
     return (
         <div id="carousel" className="relative flex justify-center w-full">
             <div className="relative flex flex-row max-w-[60vw] h-[60vh] w-full overflow-hidden">
-            <button className="absolute w-8 ml-2 left-0 inset-y-0 rounded-[50%] p-[-1] h-fit my-auto text-white z-10" onClick={()=>{stopSlider(); slideLeft()}}><ArrowLeftCircleIcon/></button>
+            <button className="absolute w-8 ml-2 left-0 inset-y-0 rounded-[50%] p-[-1] h-fit my-auto text-white z-9" onClick={()=>{stopSlider(); slideLeft()}}><ArrowLeftCircleIcon/></button>
                 {/**Right, so basically some ***hole decided hey, wouldnt it be a cool idea if the images automatically shrunk to their max resolution, 
                  *even if the user is trying to stretch it? what a ****head. Whenever stretching an image, add flex-grow/flex-shrink and make sure they are 0*/}
             {carImages.map((img, index) => {
                 return(
                     //Tailwind states that you should not use string interpolation as it will not find them and therefore not generate the corresponding css {translate-x-[50%] is fine but translate-x-[${-100 * (slide)}%] is not fine, as ${-100 * (slide)} is not a value during runtime}
                 <img onMouseEnter={()=>stopSlider()} onMouseLeave={()=>startSlider()} className={`${slide===1 ? "translate-x-[-100%]" : ""} ${slide===2 ? "translate-x-[-200%]" : ""} shrink-0 grow-0 [transition:transform_800ms] w-full`} src={img.src} alt={img.alt} key={index}></img>)})}
-                    <button className="absolute w-8 mr-2 right-0 inset-y-0 rounded-[50%] h-fit my-auto text-white z-10" onClick={()=>{stopSlider(); slideRight()}}><ArrowRightCircleIcon/></button>
+                    <button className="absolute w-8 mr-2 right-0 inset-y-0 rounded-[50%] h-fit my-auto text-white z-9" onClick={()=>{stopSlider(); slideRight()}}><ArrowRightCircleIcon/></button>
                 </div>
             <div className="absolute flex flex-row gap-2 justify-center bottom-3 right-[auto]">
                 {carImages.map((_, index) => {
--- Client/src/Components/NavBar.js
+++ Client/src/Components/NavBar.js
@@ -7,7 +7,7 @@ function NavBar(){
     const [isOpen, setIsOpen] = React.useState(false);
 
     function LinkItem(props){
-        return(<Link className="hover:text-3xl text-2xl transition-all duration-150ms flex flex-row justify-between border-b border-black" to={"/"+props.value}>{props.text}{props.img}</Link>)
+        return(<Link className="text-4xl mt-3 hover:text-5xl lg:text-3xl lg:hover:text-4xl 2xl:hover:text-3xl 2xl:text-2xl transition-all duration-150ms flex flex-row justify-between border-b border-black" to={"/"+props.value}>{props.text}{props.img}</Link>)
     }
 
     return(
@@ -15,7 +15,7 @@ function NavBar(){
             <div onClick={() => setIsOpen(!isOpen)} className="flex">
                 <Bars3Icon className="h-10 hover:cursor-pointer hover:h-12 transition-all border border-l-0 border-black bg-gray-400"/>
             </div>
-            <div className={`${isOpen ? "w-[85vw] xl:w-[13vw] h-full shadow-[0_0_10px_5px_rgba(255,255,255,.4),0_0_20px_10px_rgba(0,255,255,.2)] [transition:width_300ms_0ms]" : "border-0 h-[0vh] w-[0vw] [transition:height_0ms_300ms,width_300ms_0ms,border_0ms_300ms]"} relative rounded-sm border border-black bg-white overflow-hidden`}>
+            <div className={`${isOpen ? "w-[85vw] min-[480px]:w-[70vw] sm:w-[50vw] min-[900px]:w-[40vw] lg:w-[30vw] xl:w-[22vw] 2xl:w-[14vw] h-full shadow-[0_0_10px_5px_rgba(255,255,255,.4),0_0_20px_10px_rgba(0,255,255,.2)] [transition:width_300ms_0ms]" : "border-0 h-[0vh] w-[0vw] [transition:height_0ms_300ms,width_300ms_0ms,border_0ms_300ms]"} relative rounded-sm border border-black bg-white overflow-hidden`}>
                 <ul>
                     <li><LinkItem img={<HomeIcon className="w-8 mr-5"/>} value="" text="Home"/></li>
                     <li><LinkItem img={<VideoCameraIcon className="w-8 mr-5"/>} value="YouTubeAPI" text="YouTubeAPI"/></li>
