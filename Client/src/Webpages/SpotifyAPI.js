2f43d65c5b945a27094398b8ab98766841d2531f
CatExposure <andrenewbauer@gmail.com>
2024-08-24 11:22:49 -0400

    updated server.js with better validation and error code responses

--- Client/src/Webpages/SpotifyAPI.js
+++ Client/src/Webpages/SpotifyAPI.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {useEffect, useState} from "react";
 import Axios from 'axios';
-import {redirectUri} from "../Components/Api";
 import {apiUrl} from "../Components/Api";
 import PlaylistPopup from  "../Components/PlaylistPopup";
 <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
@@ -17,16 +16,6 @@ function SpotifyAPI(){
         getValidation();
     }, []);
 
-    function tryCatchRefresh(fn) {
-        try {
-            fn()
-        } catch (err){
-            console.log(err)
-            if (err.status === 401) {
-                refreshToken()
-            }
-        }
-    }
     function getAuth(){
             Axios({
                 method: 'post',
@@ -37,15 +26,6 @@ function SpotifyAPI(){
             })
     }
 
-    function refreshToken() {
-        Axios({
-            method: 'post',
-            url: apiUrl+'refreshToken',
-            withCredentials: true
-        }).then(() => {
-            window.location.reload();
-        });
-    }
     //if the user entered nothing or uses the * character (explained more later) then set the results state to false/none and prevents the search from running
         //as for the * character, in the actual spotify app you can search with the * character, however the API seems to despise it.
         //I don't have enough knowledge on how their search params handle *, so we will refrain from using it for now
@@ -82,7 +62,6 @@ function SpotifyAPI(){
                 data: {searchKey},
                 
             }).then((response) => {
-                console.log(response)
                 const data = response.data;
                 //if the user enteres a value and there are no artists, set the Results state to false, and set to true in any other case (if there's at least 1 artist)
                 if (data.artists.items.length === 0) {
@@ -95,36 +74,27 @@ function SpotifyAPI(){
         }
 }
 
-    //renders the genres of a particular artist in case they have more than one genre
-    const renderGenres = (item) => {
-        var genreList = "";
-        item.genres.forEach(genre => {
-            genreList += genre+", ";
-        })
-
-        genreList = genreList.slice(0, -2);
-        
-        return genreList;
-    }
-
     //maps out the artists and gives each artists a div with their id as the key, then includes an image and their name.
     //if the Results state is false, it will instead show no artists as this state would only be false if the search were to give an error or no artists
     //clicking on a artist send the user to another page to view their top-10 tracks, as well as the ability to play them
     const renderArtists = () => {
-        if (Results === "false" || Results.trim() === "") {
+        if (Results === "false" || Results.trim() === "" || !searchKey) {
             return (
                 <div></div>
             )
         } else {
             return artists.map(item => (
-                    <div className="flex border h-[20vh] border-black border-solid w-[50%] mt-5 transition-all bg-gray-600 hover:bg-gray-500 hover:h-[23vh] hover:ml-10" key={item.id} onClick={() => {
+                    <div className="flex min-w-0 border h-[20vh] border-black border-solid max-w-[95%] sm:w-[80%] lg:w-[60%] xl:w-[50%] mt-5 transition-all bg-gray-600 hover:bg-gray-500 hover:h-[23vh] hover:ml-3 lg:hover:ml-10" key={item.id} onClick={() => {
                         setArtistId(item.id)
                     }}>
                         {item.images.length ? <img className="h-full" src={item.images[0].url} alt=""/> : <div>No Image</div>}
-                        <div className="ml-5">
-                            <p className="text-6xl mb-3">{item.name}</p>
-                            <p className="text-xl">Followers: {item.followers.total} </p>
-                            <p className="text-xl">Genres: {renderGenres(item)}</p>
+                        {/**setting 'min-w-0 stops the flex item from overflowing parent 
+                         * also, I dont entirely understand this: truncate usually causes an ellipsis, but usually doesn't allow wrapping. However,
+                         * with the use of specifically whitespace-pre-wrap, its truncates only the last word if it cannot wrap anymore
+                        */}
+                        <div className="min-w-0 overflow-hidden ml-5">
+                            <p className="truncate whitespace-pre-wrap text-4xl sm:text-5xl lg:text-6xl mb-3">{item.name}</p>
+                            <p className="text-lg lg:text-xl">Followers: {item.followers.total} </p>
                         </div>
                     </div>
                 
@@ -181,9 +151,9 @@ function SpotifyAPI(){
             <div className="ml-20 flex gap-10">
             {LoginOut()}
             {validation ? 
-                <form className="flex"> 
+                <div className="flex"> 
                     <p className="text-black text-2xl mr-3">Search here: </p><input className="text-black border border-black bg-gray-500 rounded-md pl-2" type="text" placeholder="Search Artist Here" onChange={e => setSearchKey(e.target.value)}/>
-                </form>
+                </div>
 
                 : <h2>Please login to use search feature</h2>
             }
