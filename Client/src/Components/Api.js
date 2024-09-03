d5670f2e74864f010eb911b8e75a7d8c8f16cfdf
Andre Newbauer <110563280+CatExposure@users.noreply.github.com>
2024-09-03 00:17:08 -0400

    Delete Client directory

--- Client/src/Components/Api.js
+++
@@ -1,7 +0,0 @@
-const apiUrl = process.env.NODE_ENV === 'production' ? "api/v1/" : "http://localhost:5000/";
-const redirectUri = process.env.NODE_ENV === 'production' ? "https://protosite.online/" : "http://localhost:3000/";
-
-export {
-    apiUrl,
-    redirectUri
-}
