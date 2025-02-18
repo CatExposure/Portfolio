732d58c53c048e6a63104118f2c27aaf45be4afc
CatExposure <andrenewbauer@gmail.com>
2025-02-17 21:54:53 -0500

    removed unused things before copying to prod

--- home/src/Components/Carousel.js
+++ home/src/Components/Carousel.js
@@ -12,9 +12,16 @@ export default function Carousel(){
     var carImages=[
         {
             src: require("../Images/Spotify.png"),
+            href: "https://spotify.ProtoSite.online",
             alt: "Image1",
             text: "Featuring Tailwind, React, and Node, this application uses the Spotify API to allow the user to login to their account and search artists, as well as play their music with a playback GUI"
-        },
+        }, 
+        {
+            src: require("../Images/underConstruction.png"),
+            alt: "Under Contruction",
+            href: "https://e-commerce.ProtoSite.online",
+            text: "An e-commerce site that I'm currently working on! This application will feature an postgreSQL database and materialUI"
+        }
     ]
 
     let slideInterval = null;
@@ -40,7 +47,7 @@ export default function Carousel(){
     }
 
     return (
-        <div>
+        <div className='w-[85%] mx-auto'>
             <div className='flex'>
             <button className={`${darkTheme ? "text-white" : "text-black"} w-10 md:w-12 2xl:w-14 rounded-[50%] h-fit my-auto`} 
             onClick={()=>{clearTimeout(slideInterval); slideLeft()}}><ArrowLeftCircleIcon/></button>
@@ -51,20 +58,20 @@ export default function Carousel(){
                 function imgText() {
                     if (img.text) {
                         return (
-                            <p className={`${darkTheme ? "text-white" : "text-black"}`}>{img.text}</p>
+                            <p className={`${darkTheme ? "text-white" : "text-black"} text-xl text-center mt-3`}>{img.text}</p>
                         )
                     } else return (<></>)
                 }
 
                 return(
-                    <div className={`${slide===1 ? "translate-x-[-100%]" : ""} ${slide===2 ? "translate-x-[-200%]" : ""} shrink-0 grow-0 [transition:transform_800ms] w-full`}>
+                    <div className={`${slide===1 ? "translate-x-[-100%]" : ""} ${slide===2 ? "translate-x-[-200%]" : ""} shrink-0 grow-0 [transition:transform_800ms] w-fit`}>
                     {/** Tailwind states that you should not use string interpolation as it will not find them and therefore not generate the corresponding css {translate-x-[50%] is fine but translate-x-[${-100 * (slide)}%] is not fine, as ${-100 * (slide)} is not a value during runtime}*/}
-                <img 
+                <a href={img.href}><img 
                 onMouseEnter={()=> {clearTimeout(slideInterval); setAutoPlay(false)}} 
                 onMouseLeave={()=> {setAutoPlay(true)}}  
                 src={img.src} 
                 alt={img.alt} 
-                key={index}></img>
+                key={index}></img></a>
                 {imgText()}
                 </div>
                 )})}
@@ -75,7 +82,7 @@ export default function Carousel(){
         </div>
         <div className="text-center">
         {carImages.map((_, index) => {
-            return <button className={`${index===slide ? "bg-cyan-400" : "bg-white"} w-3 h-3 mx-1 rounded-[50%]`} 
+            return <button className={`${index===slide ? "bg-cyan-400" : "bg-white"} border-[1px] border-black w-3 h-3 mx-1 rounded-[50%]`} 
             key={index} 
             onClick={()=> {clearTimeout(slideInterval); setSlide(index);}}></button>
         })}
--- spotify/Client/src/Components/Api.js
+++ spotify/Client/src/Components/Api.js
@@ -1,5 +1,5 @@
-const apiUrl = process.env.NODE_ENV === 'production' ? "api/v1/" : "http://localhost:5000/";
-const redirectUri = process.env.NODE_ENV === 'production' ? "https://protosite.online/" : "http://localhost:3000/";
+const apiUrl = process.env.NODE_ENV === 'production' ? "api/" : "http://localhost:5000/";
+const redirectUri = process.env.NODE_ENV === 'production' ? "https://spotify.protosite.online/" : "http://localhost:3000/";
 
 export {
     apiUrl,
